ab5f41d0cf533aaa74ee3985cd5ff0c9d62ef488
kayes <mohashenkayes325747@gmail.com>
2026-03-09 14:01:02 +0600

    finishing my login function

--- login.js
+++ login.js
@@ -1,1 +1,17 @@
 console.log('login function')
+document.getElementById('sing-in').addEventListener('click', function () {
+  const login = document.getElementById('input-admin');
+  const adminId = login.value;
+  console.log(adminId);
+  const inputpass = document.getElementById('input-pass');
+  const password = inputpass.value;
+  console.log(password);
+  if (adminId == "admin" && password == "admin123") {
+    alert('login Success');
+    window.location.assign('/home.html')
+  } else {
+    alert('login failed');
+    return;
+  }
+
+})
